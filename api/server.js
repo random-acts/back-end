995198457220b048524ed3571a7107ac0316a9c5
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-06-24 13:58:26 -0700

    login and register working

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
 const server = express();
+const cors = require('cors');
+const userRouter = require('../auth/signUp-router')
+
 
 server.use(express.json());
+server.use(helmet());
+server.use(cors());
+server.use('/', userRouter);
 
 
 server.get('/', (req, res) => {
