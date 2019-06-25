8fb1598434674ba368b4a25bcc7c81acf1436e72
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-06-25 10:29:38 -0700

    db in env

--- knexfile.js
+++ knexfile.js
@@ -1,67 +1,67 @@
 // Update with your config settings.
-// require('dotenv').config();
-// const my_db = process.env.DATABASE_URL;
-
-// module.exports = {
-
-//   development: {
-//     client: 'sqlite3',
-//     connection: {
-//       filename: './data/random-act.db3'
-//     },
-//     useNullAsDefault: true,
-//   },
-//   pool: {
-//     afterCreate: (conn, done) => {
-//       conn.run('PRAGMA foreign_keys = ON', done);
-//     },
-//   },
-//   staging: {
-//     client: 'postgresql',
-//     connection: {
-//       database: 'my_db',
-//       user:     'username',
-//       password: 'password'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   },
-//   production: {
-//     client: 'pg',
-//     connection: my_db,
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations',
-//       directory: './migrations'
-//     },
-//     useNullAsDefault: true
-//   }
-
-// };
-
+require('dotenv').config();
+const my_db = process.env.DATABASE_URL;
 
 module.exports = {
+
   development: {
     client: 'sqlite3',
-    useNullAsDefault: true,
     connection: {
-      filename: './data/random-act.db3',
+      filename: './data/random-act.db3'
     },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
-    },
-    migrations: {
-      directory: './migrations',
+    useNullAsDefault: true,
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run('PRAGMA foreign_keys = ON', done);
     },
   },
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: my_db,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations'
+    },
+    useNullAsDefault: true
+  }
+
 };
+
+
+// module.exports = {
+//   development: {
+//     client: 'sqlite3',
+//     useNullAsDefault: true,
+//     connection: {
+//       filename: './data/random-act.db3',
+//     },
+//     pool: {
+//       afterCreate: (conn, done) => {
+//         conn.run('PRAGMA foreign_keys = ON', done);
+//       },
+//     },
+//     migrations: {
+//       directory: './migrations',
+//     },
+//   },
+// };
